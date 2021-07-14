7cfd11e55ad26afa46ea0e25af8832ea4729ac1d
Ihor RSS <superfirfox@gmail.com>
2021-07-14 17:09:05 +0300

    feat: finish task for auto test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined ||array.length === 0){
+    return 0;
+  }
+ 
+  let min = 0;
+  array.map(item => {
+    if(item < min){
+      min = item;
+    }
+  })
+  
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined ||array.length === 0){
+    return 0;
+  }
+
+  let max = 0;
+  array.map(item => {
+    if(item > max){
+      max = item;
+    }
+  })
+  
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined ||array.length === 0){
+    return 0;
+  }
+
+  return +(array.reduce((sum, item) => sum += item,0) / array.length).toFixed(2);
 }
